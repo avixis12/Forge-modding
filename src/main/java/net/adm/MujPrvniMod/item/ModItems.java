1bc5e6bf92f3283621b51c509efa4d1f2065bf9f
Avixis <adam.geryk@proton.me>
2024-05-11 13:44:10 +0200

    modified:   src/main/java/net/adm/MujPrvniMod/item/ModItems.java

--- src/main/java/net/adm/MujPrvniMod/item/ModItems.java
+++ src/main/java/net/adm/MujPrvniMod/item/ModItems.java
@@ -11,7 +11,7 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MujPrvniMod.MODID);
     public static final RegistryObject<Item> Car = ITEMS.register("Car", () -> new Item(new Item.Properties()));
 
-    public static void register(IEventBus eventBus){
+    public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
 }
