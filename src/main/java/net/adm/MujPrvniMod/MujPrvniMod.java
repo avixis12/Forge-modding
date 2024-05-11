5e66b642a80ee851a928c7440ad7f805458bdfc5
Avixis <adam.geryk@proton.me>
2024-05-11 19:15:48 +0200

    modified:   src/main/java/net/adm/MujPrvniMod/Config.java 	modified:   src/main/java/net/adm/MujPrvniMod/MujPrvniMod.java 	modified:   src/main/java/net/adm/MujPrvniMod/item/ModItems.java 	modified:   src/main/resources/assets/muj_prvni_mod/lang/en_us.json 	modified:   src/main/resources/assets/muj_prvni_mod/models/item/Car.json

--- src/main/java/net/adm/MujPrvniMod/MujPrvniMod.java
+++ src/main/java/net/adm/MujPrvniMod/MujPrvniMod.java
@@ -63,7 +63,7 @@ public class MujPrvniMod {
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS){
-            event.accept(ModItems.Car);
+            event.accept(ModItems.car);
         }
     }
 
